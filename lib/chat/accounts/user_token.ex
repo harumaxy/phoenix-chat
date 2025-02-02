5674ebd516386af9d791710aedb7c9df0a8d092f
harumaxy <harumaxy@gmail.com>
2025-02-02 23:10:37 +0900

    change uuid

--- lib/chat/accounts/user_token.ex
+++ lib/chat/accounts/user_token.ex
@@ -17,7 +17,7 @@ defmodule Chat.Accounts.UserToken do
     field :token, :binary
     field :context, :string
     field :sent_to, :string
-    belongs_to :user, Chat.Accounts.User
+    belongs_to :user, Chat.Accounts.User, type: :binary_id
 
     timestamps(type: :utc_datetime, updated_at: false)
   end
